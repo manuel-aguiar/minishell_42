d9cb1c72f0cc15a07a5b949596367027c2285b2d
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2023-09-25 08:58:07 +0000

    semicolon

--- srcs/lexer/token_list_print.c
+++ srcs/lexer/token_list_print.c
@@ -3,31 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   token_list_print.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mnascime <mnascime@student.42.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 16:47:59 by mnascime          #+#    #+#             */
-/*   Updated: 2023/09/17 15:42:23 by mnascime         ###   ########.fr       */
+/*   Updated: 2023/09/25 08:55:05 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "token_list.h"
 
-char	*token_enum_to_text(int token)
+char	*token_enum_to_text_helper(int token)
 {
-	if (token == T_OPEN_PAR)
-		return (PRINT_OPEN_PAR);
-	if (token == T_CLOSE_PAR)
-		return (PRINT_CLOSE_PAR);
-	if (token == T_OP_PIPE)
-		return (PRINT_OP_PIPE);
-	if (token == T_OP_OR)
-		return (PRINT_OP_OR);
-	if (token == T_OP_AND)
-		return (PRINT_OP_AND);
-	if (token == T_OP_AMPER)
-		return (PRINT_OP_AMPER);
-	if (token == T_INDIR_HD)
-		return (PRINT_INDIR_HD);
 	if (token == T_INDIR_OPEN)
 		return (PRINT_INDIR_OPEN);
 	if (token == T_OUTDIR_APPEND)
@@ -39,6 +25,28 @@ char	*token_enum_to_text(int token)
 	if (token == T_OPEN_PAR)
 		return (PRINT_OPEN_PAR);
 	return (NULL);
+}
+
+char	*token_enum_to_text(int token)
+{
+	if (token == T_OPEN_PAR)
+		return (PRINT_OPEN_PAR);
+	else if (token == T_CLOSE_PAR)
+		return (PRINT_CLOSE_PAR);
+	else if (token == T_OP_PIPE)
+		return (PRINT_OP_PIPE);
+	else if (token == T_OP_SEMICOL)
+		return (PRINT_OP_SEMICOL);
+	else if (token == T_OP_OR)
+		return (PRINT_OP_OR);
+	else if (token == T_OP_AND)
+		return (PRINT_OP_AND);
+	else if (token == T_OP_AMPER)
+		return (PRINT_OP_AMPER);
+	else if (token == T_INDIR_HD)
+		return (PRINT_INDIR_HD);
+	else
+		return (token_enum_to_text_helper(token));
 }
 
 void	token_list_head_print(t_token_list *list, void (*pnt)(t_token_node *))
@@ -56,6 +64,7 @@ void	token_list_head_print(t_token_list *list, void (*pnt)(t_token_node *))
 	printf("\n");
 }
 
+/*
 void	token_list_tail_print(t_token_list *list, void (*pnt)(t_token_node *))
 {
 	t_token_node	*cur;
@@ -70,6 +79,7 @@ void	token_list_tail_print(t_token_list *list, void (*pnt)(t_token_node *))
 	}
 	printf("\n");
 }
+*/
 
 void	print_token(t_token_node *token)
 {
