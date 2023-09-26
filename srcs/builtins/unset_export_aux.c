8c1e14e49669565fae82d9cff3855f5bd5cdb8d0
Margarida Aguiar <aguiar.m.margarida@gmail.com>
2023-09-26 11:27:55 +0000

    corrected export edgecase

--- srcs/builtins/unset_export_aux.c
+++ srcs/builtins/unset_export_aux.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/03 17:09:22 by mnascime          #+#    #+#             */
-/*   Updated: 2023/09/26 10:52:56 by codespace        ###   ########.fr       */
+/*   Updated: 2023/09/26 11:25:35 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,20 @@ static int	env_error(t_block *block, int is_exporting, char *arg)
 
 */
 
+static int	get_valid_len(char *arg, int is_export)
+{
+	size_t	j;
+	int		passed_equal;
+
+	j = 0;
+	passed_equal = 0;
+	while (arg[j] && (passed_equal || ft_isalpha(arg[j]) || \
+	(ft_isdigit(arg[j]) && j > 0) || arg[j] == '_' || \
+	(arg[j] == '=' && is_export && ++passed_equal)))
+		j++;
+	return (j);
+}
+
 int	get_corr_env(t_block *block, char *arg, int is_export)
 {
 	int		i;
@@ -45,14 +59,9 @@ int	get_corr_env(t_block *block, char *arg, int is_export)
 	size_t	j;
 
 	i = -1;
-	j = 0;
-	while (arg[j] && (ft_isalpha(arg[j]) \
-	|| (ft_isdigit(arg[j]) && j > 0) || arg[j] == '_' \
-	|| (arg[j] == '=' && is_export)))
-		j++;
-	if (j < ft_strlen(arg) || ft_strlen(arg) == 0 \
-	|| (arg[0] == '=' && is_export) || \
-	(ft_strrchr(arg, '=') && !is_export))
+	j = get_valid_len(arg, is_export);
+	if (j < ft_strlen(arg) || ft_strlen(arg) == 0 || (arg[0] == '=' \
+	&& is_export) || (ft_strrchr(arg, '=') && !is_export))
 		return (env_error(block, is_export, arg));
 	while (block->ms->env[++i])
 	{
