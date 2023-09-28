a12656bb0b00ca0511149c1aa7326534d304107a
Margarida Aguiar <aguiar.m.margarida@gmail.com>
2023-09-28 14:34:03 +0000

    invalid close attempted fix

--- srcs/redirections/heredoc_temp.c
+++ srcs/redirections/heredoc_temp.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 17:17:58 by mmaria-d          #+#    #+#             */
-/*   Updated: 2023/09/28 14:02:52 by codespace        ###   ########.fr       */
+/*   Updated: 2023/09/28 14:32:54 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,21 +44,15 @@ int	heredoc_temp_name(t_block *block)
 {
 	char	new[256];
 	int		found;
-	char	*pwd;
 
 	found = 0;
+	if (access("/tmp/", F_OK))
+		return (perror_msg_int("/tmp/", 0));
 	ft_memset(new, '\0', sizeof(new));
-	new[0] = '.';
-	dfs(new, sizeof(new), 1, &found);
-	pwd = getcwd(NULL, 0);
-	if (!pwd)
-		return (perror_msg_int("getcwd", 0));
-	block->here_doc = ft_triple_join(pwd, "/", new);
+	ft_memcpy(new, "/tmp/.", 6);
+	dfs(new, sizeof(new), 6, &found);
+	block->here_doc = ft_strdup(new);
 	if (!block->here_doc)
-	{
-		free(pwd);
 		return (perror_msg_int("malloc", 0));
-	}
-	free(pwd);
 	return (1);
 }
